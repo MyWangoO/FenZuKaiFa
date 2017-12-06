853f8ae79a36a29b60b6bfa731884ceb295e3e86
asus <308827964@qq.com>
2017-12-06 09:01:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/asus/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.asus.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //啥也没有
+    }
+}
---
+++ app/src/main/java/com/example/asus/myapplication/ceshi.java
@@ -0,0 +1,8 @@
+package com.example.asus.myapplication;
+
+/**
+ * Created by asus on 2017/12/6.
+ */
+
+public class ceshi {
+}
